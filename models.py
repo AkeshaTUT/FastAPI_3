d88e7112f3d52f8d4346a1b0bffdec146cd83691
AkeshaTUT <rarizoro228@gmail.com>
2025-06-07 20:24:51 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+# Akezhan Seitkassym
+
 from sqlalchemy import Column, Integer,  String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
